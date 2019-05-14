bfdfc2c9498aff85318e857a2da88f03a5eaa77b
Isaias Jair de Oliveira Junior <jrissi2008@gmail.com>
2019-05-14 19:56:24 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		Position pos = new Position(2, 5);
+		System.out.println(pos);
 	}
 
 }
