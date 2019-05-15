7aa76011fb2a2fc5be66b2804674df81d0bffb2c
Isaias Jair de Oliveira Junior <jrissi2008@gmail.com>
2019-05-15 17:42:28 -0300

    Starting to implement Board and Piece

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -5,7 +5,7 @@ public class Position {
 	private int row;
 	private int column;
 	
-	public Position(int row, int colomn) {
+	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
